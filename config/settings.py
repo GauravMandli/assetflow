899f580e070b4033ca005b185692d4c552a8bdf2
GauravMandli <gauravmandli2004@gmail.com>
2026-07-12 12:48:58 +0530

    Initial Django project setup

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'dashboard',
+    'assets',
+    'allocation',
+    'booking',
+    'maintenance',
+    'audit',
+    'notifications',
 ]
 
 MIDDLEWARE = [
